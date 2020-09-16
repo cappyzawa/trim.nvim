658ed81ddb6186ad7f3def0ce5ee1bae02397121
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2020-09-16 16:51:50 +0900

    enable to remove whitespace

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # trim.nvim
-This plugin trims whitespace and trailing lines
+
+This plugin trims whitespace and trailing lines.
+
+**TODO**
+
+- [x] whitespace
+- [ ] trailing lines
+
+## How to install
+
+```vim
+Plug 'cappyzawa/trim.nvim'
+```
+
+## How to setup
+
+```vim
+lua << EOF
+lua require('trim').setup()
+EOF
+```
--- lua/trim/trimmer.lua
+++ lua/trim/trimmer.lua
@@ -2,9 +2,15 @@ local vim = vim
 local api = vim.api
 local trimmer = {
   trim = function (arg, startLine, endLine)
-    print("arg", arg)
-    print("start", startLine)
-    print("end", endLine)
+    startLine = startLine - 1
+    local bufnr = api.nvim_get_current_buf()
+    local lines = api.nvim_buf_get_lines(bufnr, startLine, endLine, true)
+    local newLines = {}
+    for _, v in ipairs(lines) do
+      local newLine = string.gsub(v, "%s+$", "")
+      table.insert(newLines, newLine)
+    end
+    api.nvim_buf_set_lines(bufnr, startLine, endLine, true, newLines)
   end
 }
 
