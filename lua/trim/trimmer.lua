eda0fb525d0c814c4454b3170575218b0aaf05e8
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2020-09-16 20:06:34 +0900

    restor cursor after fixing

--- lua/trim/trimmer.lua
+++ lua/trim/trimmer.lua
@@ -4,6 +4,8 @@ local trimmer = {
   trim = function (arg, startLine, endLine)
     startLine = startLine - 1
     local bufnr = api.nvim_get_current_buf()
+    local win = api.nvim_get_current_win()
+    local pos = api.nvim_win_get_cursor(win)
     local lines = api.nvim_buf_get_lines(bufnr, startLine, endLine, true)
     local newLines = {}
     local endIndex = #lines
@@ -19,6 +21,8 @@ local trimmer = {
       table.insert(newLines, newLine)
     end
     api.nvim_buf_set_lines(bufnr, startLine, endLine, true, newLines)
+    if pos[1] >= #newLines then pos[1] = #newLines end
+    api.nvim_win_set_cursor(win, pos)
   end
 }
 
