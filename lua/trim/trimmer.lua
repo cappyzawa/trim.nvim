a37abec64341e088beac639550e32c0a2f6252c2
Shu Kutsuzawa <cappyzawa@gmail.com>
2021-06-23 13:23:25 +0900

    fix

--- lua/trim/trimmer.lua
+++ lua/trim/trimmer.lua
@@ -2,7 +2,9 @@ local vim = vim
 local api = vim.api
 local trimmer = {
   trim = function (arg, startLine, endLine)
-    -- reference: https://stackoverflow.com/questions/7495932/how-can-i-trim-blank-lines-at-the-end-of-file-in-vim    
+    -- reference: https://vim.fandom.com/wiki/Remove_unwanted_spaces
+    api.nvim_exec([[silent! %s/\s\+$//e]], false)
+    -- reference: https://stackoverflow.com/questions/7495932/how-can-i-trim-blank-lines-at-the-end-of-file-in-vim
     api.nvim_exec([[silent! %s#\($\n\s*\)\+\%$##]], false)
   end
 }
