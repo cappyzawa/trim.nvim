3f30c96551e1d3ef7aa4107d2fbbb421ec66d04b
vzytoi <65293868+vzytoi@users.noreply.github.com>
2021-08-23 04:52:30 +0200

    Merge several new lines in a row into one. (#2)

--- lua/trim/trimmer.lua
+++ lua/trim/trimmer.lua
@@ -6,6 +6,8 @@ local trimmer = {
     api.nvim_exec([[silent! %s/\s\+$//e]], false)
     -- reference: https://stackoverflow.com/questions/7495932/how-can-i-trim-blank-lines-at-the-end-of-file-in-vim
     api.nvim_exec([[silent! %s#\($\n\s*\)\+\%$##]], false)
+    -- reference: https://unix.stackexchange.com/questions/12812/replacing-multiple-blank-lines-with-a-single-blank-line-in-vim-sed
+    api.nvim_exec([[silent! %s/\(\n\n\)\n\+/\1/]], false)
   end
 }
 
