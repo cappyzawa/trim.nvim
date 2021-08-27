1ad342cd50dc312ac898a7600164bf6b1cc7c913
vzytoi <65293868+vzytoi@users.noreply.github.com>
2021-08-27 15:52:36 +0200

    Trim empty lines from the top of the buffer
    The plugin isn't currently removing empty lines from the top of the buffer.
    I don't put references because I made this regex myself.
    Have a good day!

--- lua/trim/trimmer.lua
+++ lua/trim/trimmer.lua
@@ -6,8 +6,10 @@ local trimmer = {
     api.nvim_exec([[silent! %s/\s\+$//e]], false)
     -- reference: https://stackoverflow.com/questions/7495932/how-can-i-trim-blank-lines-at-the-end-of-file-in-vim
     api.nvim_exec([[silent! %s#\($\n\s*\)\+\%$##]], false)
+    api.nvim_exec([[silent! %s/\%^\n\+//]], false)
     -- reference: https://unix.stackexchange.com/questions/12812/replacing-multiple-blank-lines-with-a-single-blank-line-in-vim-sed
     api.nvim_exec([[silent! %s/\(\n\n\)\n\+/\1/]], false)
+    
   end
 }
 
