27850d57fc885381e4963d3f02081f32b8e90957
vzytoi <65293868+vzytoi@users.noreply.github.com>
2021-08-27 17:53:59 +0200

    Restore the position (#4)
    I noticed that on a fairly large file, the cursor position was changed and not restored
    see :h winrestview

--- lua/trim/trimmer.lua
+++ lua/trim/trimmer.lua
@@ -2,6 +2,7 @@ local vim = vim
 local api = vim.api
 local trimmer = {
   trim = function (arg, startLine, endLine)
+    local save = vim.fn.winsaveview()
     -- reference: https://vim.fandom.com/wiki/Remove_unwanted_spaces
     api.nvim_exec([[silent! %s/\s\+$//e]], false)
     -- reference: https://stackoverflow.com/questions/7495932/how-can-i-trim-blank-lines-at-the-end-of-file-in-vim
@@ -9,6 +10,7 @@ local trimmer = {
     api.nvim_exec([[silent! %s/\%^\n\+//]], false)
     -- reference: https://unix.stackexchange.com/questions/12812/replacing-multiple-blank-lines-with-a-single-blank-line-in-vim-sed
     api.nvim_exec([[silent! %s/\(\n\n\)\n\+/\1/]], false)
+    vim.fn.winrestview(save)
   end
 }
 
