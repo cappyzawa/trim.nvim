e67363d976c022424b8164deffd34345009a9abf
vzytoi <65293868+vzytoi@users.noreply.github.com>
2022-02-28 16:39:00 +0100

    use keepjumps and keeppatterns (#10)
    see `:h keepjumps` && `:h keeppatterns`

--- lua/trim/trimmer.lua
+++ lua/trim/trimmer.lua
@@ -7,7 +7,7 @@ local trimmer = {}
 trimmer.trim = function()
     local save = vim.fn.winsaveview()
     for _, v in pairs(cfg.patterns) do
-        api.nvim_exec(string.format("silent! %s", v), false)
+        api.nvim_exec(string.format("keepjumps keeppatterns silent! %s", v), false)
     end
     vim.fn.winrestview(save)
 end
