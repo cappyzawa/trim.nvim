8c29b69902057f4dacf8af2f3bb712111ae3c81b
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2024-05-24 20:37:41 +0200

    Add trim of changed lines only

--- lua/trim/config.lua
+++ lua/trim/config.lua
@@ -9,6 +9,7 @@ local default_config = {
   trim_trailing = true,
   trim_last_line = true,
   trim_first_line = true,
+  trim_changed_only = false,
   highlight = false,
   highlight_bg = '#ff0000',
   highlight_ctermbg = 'red',
@@ -21,6 +22,13 @@ function M.setup(opts)
   if opts.disable and not opts.ft_blocklist then
     vim.notify('`disable` is deprecated, use `ft_blocklist` instead', vim.log.levels.WARN, { title = 'trim.nvim' })
     opts.ft_blocklist = opts.disable
+  end
+
+  if opts.trim_changed_only and vim.fn.executable('diff') == 0 then
+    vim.notify('`trim_changed_only` cannot function without a `diff` binary available in PATH. Disabling trimming.', { title = 'trim.nvim' })
+    opts.trim_first_line = false
+    opts.trim_last_line = false
+    opts.trim_trailing = false
   end
 
   M.config = vim.tbl_deep_extend('force', default_config, opts)
--- lua/trim/trimmer.lua
+++ lua/trim/trimmer.lua
@@ -3,11 +3,81 @@ local api = vim.api
 
 local trimmer = {}
 
+function trimmer.changed_blocks()
+  local filename = vim.api.nvim_buf_get_name(0)
+
+  if vim.fn.filereadable(filename) == 0 then
+    return {1, vim.fn.line('$')}
+  end
+
+  if vim.bo.modified then
+    local diff_bits = {
+      "diff",
+      "-a",
+      "--unchanged-group-format=",
+      "--old-group-format=",
+      "--new-group-format=%dF,%dL ",
+      "--changed-group-format=%dF,%dL ",
+      filename,
+      "-"
+    }
+
+    local format = vim.api.nvim_buf_get_option(0, 'fileformat')
+    local line_ending = line_endings[format]
+
+    local file_content = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, true), line_ending) .. line_ending
+    local call_result = vim.system(diff_bits, {stdin = file_content, text = true}):wait()
+
+    local trimmed_output = vim.fn.trim(call_result.stdout)
+    local changes_list = vim.split(trimmed_output, ' ')
+    local result = vim.tbl_map(function(val)
+      return vim.split(val, ',')
+    end, changes_list)
+
+    return result
+  end
+
+  return {}
+end
+
+local format_line_endings = {
+  unix = "\n",
+  dos = "\r\n",
+  mac = "\r",
+}
+
 function trimmer.trim()
   local config = require('trim.config').get()
   local save = vim.fn.winsaveview()
-  for _, v in ipairs(config.patterns) do
-    api.nvim_exec(string.format('keepjumps keeppatterns silent! %s', v), false)
+  if config.trim_changed_only then
+    local line_trailing_pattern = [[s/\s\+$//e]]
+    local changed_blocks = trimmer.changed_blocks()
+
+    -- Trim ends of all changed lines
+    local num_changed_lines = 0
+    for _, range in ipairs(changed_blocks) do
+      local command = string.format('keepjumps keeppatterns silent! %s,%s%s', range[1], range[2], line_trailing_pattern)
+      api.nvim_exec2(command)
+      num_changed_lines = num_changed_lines + 1
+    end
+
+    -- NOTE: order is important, do trailing first, then leading, otherwise the
+    -- line ranges might not be accurate anymore
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if config.trim_last_line and num_changed_lines > 0 and changed_blocks[num_changed_lines][2] == line_count then
+      -- Last changed range extends to EOF -> We can trim trailing lines
+      api.nvim_exec2([[keepjumps keeppatterns silent! %s/\($\n\s*\)\+\%$//]])
+    end
+
+    if config.trim_first_line and num_changed_lines > 0 and changed_blocks[1][1] == 1 then
+      -- First changed range starts at first line -> We can trim leading lines
+      api.nvim_exec2([[keepjumps keeppatterns silent! %s/\%^\n\+//]])
+    end
+  else
+    -- Trim globally
+    for _, v in ipairs(config.patterns) do
+      api.nvim_exec2(string.format('keepjumps keeppatterns silent! %s', v), false)
+    end
   end
   vim.fn.winrestview(save)
 end
