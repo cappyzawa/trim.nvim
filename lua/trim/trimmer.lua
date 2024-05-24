08ee0b3260ae449c34e9765fedb38633bbe10f65
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2024-05-24 21:29:18 +0200

    Add missing work

--- lua/trim/trimmer.lua
+++ lua/trim/trimmer.lua
@@ -3,6 +3,12 @@ local api = vim.api
 
 local trimmer = {}
 
+local format_line_endings = {
+  unix = "\n",
+  dos = "\r\n",
+  mac = "\r",
+}
+
 function trimmer.changed_blocks()
   local filename = vim.api.nvim_buf_get_name(0)
 
@@ -23,28 +29,22 @@ function trimmer.changed_blocks()
     }
 
     local format = vim.api.nvim_buf_get_option(0, 'fileformat')
-    local line_ending = line_endings[format]
+    local line_ending = format_line_endings[format]
 
     local file_content = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, true), line_ending) .. line_ending
     local call_result = vim.system(diff_bits, {stdin = file_content, text = true}):wait()
 
     local trimmed_output = vim.fn.trim(call_result.stdout)
     local changes_list = vim.split(trimmed_output, ' ')
-    local result = vim.tbl_map(function(val)
-      return vim.split(val, ',')
-    end, changes_list)
+    local split_fn = function(val)
+      return vim.split(val, ",")
+    end
 
-    return result
+    return vim.tbl_map(split_fn, changes_list)
   end
 
   return {}
 end
-
-local format_line_endings = {
-  unix = "\n",
-  dos = "\r\n",
-  mac = "\r",
-}
 
 function trimmer.trim()
   local config = require('trim.config').get()
@@ -54,29 +54,29 @@ function trimmer.trim()
     local changed_blocks = trimmer.changed_blocks()
 
     -- Trim ends of all changed lines
-    local num_changed_lines = 0
+    local num_changed_blocks = 0
     for _, range in ipairs(changed_blocks) do
       local command = string.format('keepjumps keeppatterns silent! %s,%s%s', range[1], range[2], line_trailing_pattern)
-      api.nvim_exec2(command)
-      num_changed_lines = num_changed_lines + 1
+      api.nvim_exec2(command, {output = false})
+      num_changed_blocks = num_changed_blocks + 1
     end
 
     -- NOTE: order is important, do trailing first, then leading, otherwise the
     -- line ranges might not be accurate anymore
     local line_count = vim.api.nvim_buf_line_count(0)
-    if config.trim_last_line and num_changed_lines > 0 and changed_blocks[num_changed_lines][2] == line_count then
+    if config.trim_last_line and num_changed_blocks > 0 and tonumber(changed_blocks[num_changed_blocks][2]) == line_count then
       -- Last changed range extends to EOF -> We can trim trailing lines
-      api.nvim_exec2([[keepjumps keeppatterns silent! %s/\($\n\s*\)\+\%$//]])
+      api.nvim_exec2([[keepjumps keeppatterns silent! %s/\($\n\s*\)\+\%$//]], {output = false})
     end
 
-    if config.trim_first_line and num_changed_lines > 0 and changed_blocks[1][1] == 1 then
+    if config.trim_first_line and num_changed_blocks > 0 and tonumber(changed_blocks[1][1]) == 1 then
       -- First changed range starts at first line -> We can trim leading lines
-      api.nvim_exec2([[keepjumps keeppatterns silent! %s/\%^\n\+//]])
+      api.nvim_exec2([[keepjumps keeppatterns silent! %s/\%^\n\+//]], {output = false})
     end
   else
     -- Trim globally
     for _, v in ipairs(config.patterns) do
-      api.nvim_exec2(string.format('keepjumps keeppatterns silent! %s', v), false)
+      api.nvim_exec2(string.format('keepjumps keeppatterns silent! %s', v), {output = false})
     end
   end
   vim.fn.winrestview(save)
