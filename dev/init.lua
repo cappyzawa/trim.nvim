07c09ebd4b1c75ae9645cb3ef46a5d71ec578e5f
cappyzawa <cappyzawa@gmail.com>
2025-12-30 22:30:36 +0900

    Add development environment for manual testing
    Allow developers to test the plugin in an isolated environment
    without affecting their personal Neovim configuration.
    
    Signed-off-by: cappyzawa <cappyzawa@gmail.com>

---
+++ dev/init.lua
@@ -0,0 +1,45 @@
+-- Minimal config for development and manual testing
+-- Usage: nvim --clean -u dev/init.lua [file]
+
+local root = vim.fn.fnamemodify(debug.getinfo(1, 'S').source:sub(2), ':p:h:h')
+
+-- Setup runtimepath
+vim.opt.runtimepath:prepend(root)
+vim.opt.runtimepath:append(root .. '/after')
+
+-- Basic settings
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Setup trim.nvim with highlight enabled for easier testing
+require('trim').setup({
+  highlight = true,
+  highlight_bg = '#ff0000',
+})
+
+-- Create sample file with trailing whitespace for testing :w behavior
+local sample_file = root .. '/dev/sample.txt'
+local sample_content = {
+  'Line with trailing spaces   ',
+  '',
+  'Normal line',
+  'Another line with tabs\t\t',
+  '',
+  '',
+}
+
+local function create_sample()
+  local f = io.open(sample_file, 'w')
+  if f then
+    f:write(table.concat(sample_content, '\n'))
+    f:close()
+  end
+end
+
+-- If no file argument provided, create and open sample file
+if vim.fn.argc() == 0 then
+  create_sample()
+  vim.cmd.edit(sample_file)
+end
